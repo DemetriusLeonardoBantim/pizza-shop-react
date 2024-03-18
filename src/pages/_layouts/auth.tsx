ae6a0d5d7e4cf32fdc3bdeaa459850aeafde88a3
demetriusleonardobantim <demetriusbantimvas@gmail.com>
2024-03-18 15:55:36 -0300

    refactor: create app auth layout with outlet

--- src/pages/_layouts/auth.tsx
+++ src/pages/_layouts/auth.tsx
@@ -1,7 +1,13 @@
+import { Outlet } from 'react-router-dom'
+
 export function AuthLayout() {
     return (
         <div>
-            <h1>Cabeçalho</h1>
+            <h1>App</h1>
+
+            <div>
+                <Outlet />
+            </div>
         </div>
     )
 }
