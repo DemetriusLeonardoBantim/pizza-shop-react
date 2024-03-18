c9e8ac581a0036b07d27348deb1f9d297fde76ef
demetriusleonardobantim <demetriusbantimvas@gmail.com>
2024-03-18 16:53:46 -0300

    refactor: add prop to sonner

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,12 @@ import './global.css'
 import { RouterProvider } from 'react-router-dom'
 import { router } from './routes'
 import { HelmetProvider, Helmet } from 'react-helmet-async'
+import { Toaster } from 'sonner'
 
 export function App() {
   return (
     <HelmetProvider>
+      <Toaster richColors />
       <Helmet titleTemplate='%s | pizza.shop' />
       <RouterProvider router={router} />
     </HelmetProvider>
