6f7ec55d4a2cb857891fab929998826d43af75b8
demetriusleonardobantim <demetriusbantimvas@gmail.com>
2024-03-18 15:33:31 -0300

    chore: configure lib components json

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './global.css'
+import { Button } from './components/ui/button'
+
+export function App() {
+  return (
+    <div className='flex justify-center items-center h-screen'>
+      <Button>
+        Enviar
+      </Button>
+    </div>
+  )
+}
