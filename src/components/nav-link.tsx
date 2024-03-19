855aade0c948dd5ca2f7615bda06f8eb293784fa
demetriusleonardobantim <demetriusbantimvas@gmail.com>
2024-03-19 08:54:10 -0300

    feature: create nav link component

---
+++ src/components/nav-link.tsx
@@ -0,0 +1,12 @@
+import { ComponentProps } from 'react'
+import { LinkProps } from 'react-router-dom'
+
+export type NavLinkProps = LinkProps
+
+export function NavLink(props: NavLinkProps) {
+    return (
+        <div>
+
+        </div>
+    )
+}
