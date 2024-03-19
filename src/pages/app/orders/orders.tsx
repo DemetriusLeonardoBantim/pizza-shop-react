b0ca511521121aee8f86ea99e58dce02c6c0318b
demetriusleonardobantim <demetriusbantimvas@gmail.com>
2024-03-19 16:37:48 -0300

    refactor: remove inputs

--- src/pages/app/orders/orders.tsx
+++ src/pages/app/orders/orders.tsx
@@ -22,10 +22,6 @@ export function Orders() {
                 <h1 className="text-3xl font-bold tracking-tighter">Pedidos</h1>
             </div>
             <div className="space-y-2.5">
-                <form className="flex items-center gap-2">
-                    <span className="text-sm font-semibold">Filtros:</span>
-                    <Input placeholder="Nome do cliente" className="h-8 w-[320px]" />
-                </form>
                 <OrderTableFilters />
                 <div className="border rounded-md">
                     <Table>
