6e0069f3a4a83c8aee8a8e0eda41d061f3eac02e
demetriusleonardobantim <demetriusbantimvas@gmail.com>
2024-03-19 08:50:50 -0300

    feature: create header component

---
+++ src/components/header.tsx
@@ -0,0 +1,29 @@
+import { Home, Pizza, UtensilsCrossed } from "lucide-react";
+import { Separator } from "./ui/separator";
+import { Link } from "react-router-dom";
+
+export interface HeaderProps { }
+
+export function Header(props: HeaderProps) {
+    return (
+        <div className="border-b">
+            <div className="flex h-16 items-center gap-6 px-6">
+                <Pizza className="h-6 w-6" />
+
+                <Separator orientation="vertical" className="h-6" />
+
+                <nav className="flex items-center space-x-4 lg:space-x-6">
+                    <Link to="/">
+                        <Home className="h-4 w-4" />
+                        Inicio
+                    </Link>
+
+                    <Link to="/">
+                        <UtensilsCrossed className="h-4 w-4" />
+                        Pedidos
+                    </Link>
+                </nav>
+            </div>
+        </div>
+    )
+}
