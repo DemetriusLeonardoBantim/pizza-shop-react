c3cee052cac6e436ac150017055c58531d3bf60e
demetriusleonardobantim <demetriusbantimvas@gmail.com>
2024-05-06 12:59:21 -0300

    refactor: page dashboard

--- src/pages/app/dashboard/dashboard.tsx
+++ src/pages/app/dashboard/dashboard.tsx
@@ -1,6 +1,8 @@
-import { CardHeader, Card, CardTitle, CardContent } from '@/components/ui/card'
-import { DollarSign } from 'lucide-react'
 import { Helmet } from 'react-helmet-async'
+import { MonthRevenueCard } from './month-revenue-card'
+import { MonthOrdersAmountCard } from './month-orders-amount-card'
+import { DayOrdersAmountCard } from './day-orders-amount-card'
+import { MonthCanceledOrdersAmountCard } from './month-canceled-orders-amount'
 
 export function Dashboard() {
     return (
@@ -11,26 +13,10 @@ export function Dashboard() {
                 <h1 className="text-3xl font-bold tracking-tighter">Dashboard</h1>
 
                 <div className='grid grid-cols-4 gap-4 justify-between pb-2'>
-                    <Card>
-                        <CardHeader className='flex-row items-center justify-between  space-y-0 pb-2'>
-                            <CardTitle className="text-base font-semibold">
-                                Receita Total (mêS)
-                            </CardTitle>
-                            <DollarSign className='h-4 w-4 text-muted-foreground' />
-                        </CardHeader>
-
-                        <CardContent className='space-y-1'>
-                            <span className='text-2xl font-bold tracking-tighter'>
-                                R$ 1248,6-
-                            </span>
-                            <p className='text-xs text-muted-foreground'>
-                                <span className='text-emerald-500 dark:text-teal-400'>
-                                    +2%
-                                </span>{' '}
-                                em relação ao mês passado
-                            </p>
-                        </CardContent>
-                    </Card>
+                    <MonthRevenueCard />
+                    <MonthOrdersAmountCard />
+                    <DayOrdersAmountCard />
+                    <MonthCanceledOrdersAmountCard />
                 </div>
             </div>
         </>
