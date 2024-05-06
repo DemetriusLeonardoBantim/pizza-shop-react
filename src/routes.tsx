52d13db532de97015c9c2add0942336f63cd0a6a
demetriusleonardobantim <demetriusbantimvas@gmail.com>
2024-05-06 17:44:26 -0300

    feature: implement page not found in routes

--- src/routes.tsx
+++ src/routes.tsx
@@ -5,8 +5,9 @@ import { AppLayout } from './pages/_layouts/app'
 import { AuthLayout } from './pages/_layouts/auth'
 import { SignUp } from './pages/auth/sign-up'
 import { Orders } from './pages/app/orders/orders'
+import { NotFound } from './pages/404'
 
 export const router = createBrowserRouter([
-    { path: '/', element: <AppLayout />, children: [{ path: '/', element: <Dashboard /> }, { path: '/orders', element: <Orders /> }] },
+    { path: '/', errorElement: <NotFound />, element: <AppLayout />, children: [{ path: '/', element: <Dashboard /> }, { path: '/orders', element: <Orders /> }] },
     { path: '/', element: <AuthLayout />, children: [{ path: '/sign-in', element: <SignIn /> }, { path: '/sign-up', element: <SignUp /> }] },
 ])
