b57a8e3107c89131bce7dc6d3cdf6c1086a73dd7
demetriusleonardobantim <demetriusbantimvas@gmail.com>
2024-05-13 15:51:24 -0300

    refactor: update name

---
+++ src/app.tsx
@@ -0,0 +1,22 @@
+import './global.css'
+import { RouterProvider } from 'react-router-dom'
+import { router } from './routes'
+import { HelmetProvider, Helmet } from 'react-helmet-async'
+import { Toaster } from 'sonner'
+import { ThemeProvider } from './components/theme/theme-provider'
+import { QueryClientProvider } from '@tanstack/react-query'
+import { queryClient } from './lib/react-query'
+
+export function App() {
+  return (
+    <HelmetProvider>
+      <ThemeProvider storageKey='pizzashop-themes' defaultTheme='dark'>
+        <Toaster richColors />
+        <Helmet titleTemplate='%s | pizza.shop' />
+        <QueryClientProvider client={queryClient}>
+          <RouterProvider router={router} />
+        </QueryClientProvider>
+      </ThemeProvider>
+    </HelmetProvider>
+  )
+}
