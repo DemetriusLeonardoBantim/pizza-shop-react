2bbb43e5ceb6b5ebd4d186f07a490ee7e2f77964
demetriusleonardobantim <demetriusbantimvas@gmail.com>
2024-05-14 08:48:02 -0300

    feature: add form in StoreProfileDialog

--- src/components/store-profile-dialog.tsx
+++ src/components/store-profile-dialog.tsx
@@ -1,6 +1,8 @@
 import { DialogDescription, DialogTitle } from '@radix-ui/react-dialog'
 import { DialogContent, DialogFooter, DialogHeader } from './ui/dialog'
 import { Button } from './ui/button'
+import { Label } from './ui/label'
+import { Input } from './ui/input'
 
 
 export function StoreProfileDialog() {
@@ -12,9 +14,35 @@ export function StoreProfileDialog() {
             </DialogHeader>
 
 
+
+            <form>
+                <div className=' space-y-4 py-4'>
+                    <div className='grid grid-cols-4 items-center gap-4'>
+                        <Label className='text-right'>
+                            Nome
+                        </Label>
+                        <Input className='col-span-3' id='name' />
+                    </div>
+
+                    <div className='grid grid-cols-4 items-center gap-4'>
+                        <Label className='text-right'>
+                            Descrição
+                        </Label>
+                        <Input className='col-span-3' id='name' />
+                    </div>
+
+
+                    <div className='grid grid-cols-4 items-center gap-4'>
+                        <Label className='text-right'>
+                            Nome
+                        </Label>
+                        <Input className='col-span-3' id='name' />
+                    </div>
+                </div>
+            </form>
             <DialogFooter>
-                <Button>Cancelar</Button>
-                <Button type="submit">Salvar</Button>
+                <Button variant="ghost">Cancelar</Button>
+                <Button type="submit" variant="default">Salvar</Button>
             </DialogFooter>
         </DialogContent>
     )
