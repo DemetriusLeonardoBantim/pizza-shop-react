2b4575624fee2b1ad956c38a36515ea8394d2c53
demetriusleonardobantim <demetriusbantimvas@gmail.com>
2024-05-13 21:02:19 -0300

    chore: update config axios

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -3,5 +3,6 @@ import axios from 'axios'
 import { env } from '@/env'
 
 export const api = axios.create({
-    baseURL: 'http://localhost:3333'
+    baseURL: 'http://localhost:3333',
+    withCredentials: true
 })
