78138749380f6beef6958a1763389d7db5a6dbbd
demetriusleonardobantim <demetriusbantimvas@gmail.com>
2024-05-14 08:43:27 -0300

    refactor: update style StoreProfileDialog

--- src/components/store-profile-dialog.tsx
+++ src/components/store-profile-dialog.tsx
@@ -1,7 +1,21 @@
-import { DialogContent } from './ui/dialog'
+import { DialogDescription, DialogTitle } from '@radix-ui/react-dialog'
+import { DialogContent, DialogFooter, DialogHeader } from './ui/dialog'
+import { Button } from './ui/button'
+
 
 export function StoreProfileDialog() {
     return (
-        <DialogContent>Teste</DialogContent>
+        <DialogContent>
+            <DialogHeader>
+                <DialogTitle>Perfil da loja</DialogTitle>
+                <DialogDescription>Atualize as informações do seu estabelecimento visíveis ao seu cliente</DialogDescription>
+            </DialogHeader>
+
+
+            <DialogFooter>
+                <Button>Cancelar</Button>
+                <Button type="submit">Salvar</Button>
+            </DialogFooter>
+        </DialogContent>
     )
 }
