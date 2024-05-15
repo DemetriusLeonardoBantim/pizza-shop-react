908efa599eec3752e56824b72cf9f71cd7dde84d
demetriusleonardobantim <demetriusbantimvas@gmail.com>
2024-05-15 15:09:15 -0300

    feature: use hook form in filters

--- src/pages/app/orders/order-table-filters.tsx
+++ src/pages/app/orders/order-table-filters.tsx
@@ -10,7 +10,7 @@ import {
     SelectValue,
 } from '@/components/ui/select'
 import { z } from 'zod'
-import { useForm } from 'react-hook-form'
+import { Controller, useForm } from 'react-hook-form'
 import { zodResolver } from '@hookform/resolvers/zod'
 
 const orderFiltersSchema = z.object({
@@ -22,7 +22,7 @@ const orderFiltersSchema = z.object({
 type OrderFiltersSchema = z.infer<typeof orderFiltersSchema>
 
 export function OrderTableFilters() {
-    const { register, handleSubmit } = useForm<OrderFiltersSchema>({
+    const { register, handleSubmit, control } = useForm<OrderFiltersSchema>({
         resolver: zodResolver(orderFiltersSchema)
     })
 
@@ -33,21 +33,38 @@ export function OrderTableFilters() {
     return (
         <form className="flex items-center gap-2" onSubmit={handleSubmit(handleFilter)}>
             <span className="text-sm font-semibold">Filtros:</span>
-            <Input placeholder="ID do pedido" className="h-8 w-auto" />
-            <Input placeholder="Nome do cliente" className="h-8 w-[320px]" />
-            <Select defaultValue="all">
-                <SelectTrigger className="h-8 w-[180px]">
-                    <SelectValue />
-                </SelectTrigger>
-                <SelectContent>
-                    <SelectItem value="all">Todos status</SelectItem>
-                    <SelectItem value="pending">Pendente</SelectItem>
-                    <SelectItem value="canceled">Cancelado</SelectItem>
-                    <SelectItem value="processing">Em preparo</SelectItem>
-                    <SelectItem value="delivering">Em entrega</SelectItem>
-                    <SelectItem value="delivered">Entregue</SelectItem>
-                </SelectContent>
-            </Select>
+            <Input placeholder="ID do pedido" className="h-8 w-auto" {...register('orderId')} />
+
+            <Input placeholder="Nome do cliente" className="h-8 w-[320px]"  {...register('customerName')} />
+            <Controller
+                name="status"
+                control={control}
+                render={({ field: { name, onChange, value, disabled } }) => {
+                    return (
+                        <Select
+                            defaultValue="all"
+                            name={name}
+                            onValueChange={onChange}
+                            value={value}
+                            disabled={disabled}
+                        >
+                            <SelectTrigger className="h-8 w-[180px]">
+                                <SelectValue />
+                            </SelectTrigger>
+                            <SelectContent>
+                                <SelectItem value="all">Todos status</SelectItem>
+                                <SelectItem value="pending">Pendente</SelectItem>
+                                <SelectItem value="canceled">Cancelado</SelectItem>
+                                <SelectItem value="processing">Em preparo</SelectItem>
+                                <SelectItem value="delivering">Em entrega</SelectItem>
+                                <SelectItem value="delivered">Entregue</SelectItem>
+                            </SelectContent>
+                        </Select>
+                    )
+                }}
+            ></Controller>
+
+
             <Button variant="secondary" size="xs" type="submit">
                 <Search className="mr-2 h-4 w-4" />
                 Filtrar resultados
@@ -56,6 +73,6 @@ export function OrderTableFilters() {
                 <X className="mr-2 h-4 w-4" />
                 Remover filtros
             </Button>
-        </form>
+        </form >
     )
 }
