80a2e96f149794be04ea2c1281227899c19f8c76
demetriusleonardobantim <demetriusbantimvas@gmail.com>
2024-05-18 13:41:34 -0300

    chore: update files route

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,13 +1,41 @@
 import { createBrowserRouter } from 'react-router-dom'
-import { Dashboard } from './pages/app/dashboard/dashboard'
-import { SignIn } from './pages/auth/sign-in'
+
 import { AppLayout } from './pages/_layouts/app'
 import { AuthLayout } from './pages/_layouts/auth'
-import { SignUp } from './pages/auth/sign-up'
-import { Orders } from './pages/app/orders/orders'
 import { NotFound } from './pages/404'
+import { Dashboard } from './pages/app/dashboard/dashboard'
+import { Orders } from './pages/app/orders/orders'
+import { SignIn } from './pages/auth/sign-in'
+import { SignUp } from './pages/auth/sign-up'
 
 export const router = createBrowserRouter([
-    { path: '/', errorElement: <NotFound />, element: <AppLayout />, children: [{ path: '/', element: <Dashboard /> }, { path: '/orders', element: <Orders /> }] },
-    { path: '/', element: <AuthLayout />, children: [{ path: '/sign-in', element: <SignIn /> }, { path: '/sign-up', element: <SignUp /> }] },
+    {
+        path: '/',
+        element: <AppLayout />,
+        errorElement: <NotFound />,
+        children: [
+            {
+                path: '/',
+                element: <Dashboard />,
+            },
+            {
+                path: '/orders',
+                element: <Orders />,
+            },
+        ],
+    },
+    {
+        path: '/',
+        element: <AuthLayout />,
+        children: [
+            {
+                path: '/sign-in',
+                element: <SignIn />,
+            },
+            {
+                path: '/sign-up',
+                element: <SignUp />,
+            },
+        ],
+    },
 ])
