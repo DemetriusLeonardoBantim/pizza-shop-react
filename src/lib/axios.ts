976610e7ff8fe336f6ab52f3cbd7be01a8fdbfcd
demetriusleonardobantim <demetriusbantimvas@gmail.com>
2024-05-19 18:01:07 -0300

    feature: update fetch time

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,8 +1,14 @@
 import axios from 'axios'
 
-import { env } from '@/env'
-
 export const api = axios.create({
     baseURL: 'http://localhost:3333',
     withCredentials: true
+})
+
+api.interceptors.request.use(async (config) => {
+    await new Promise((resolve) =>
+        setTimeout(resolve, Math.round(Math.random() * 3000)),
+    )
+
+    return config
 })
